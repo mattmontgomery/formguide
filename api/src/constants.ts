429c6e3196e49bcac28dbda46dfb12198308f55b
Matt Montgomery <mmontgomery@deseretdigital.com>
2022-04-04 11:56:08 -0600

    refactor: ♻️ moves api to google cloud functions

---
+++ api/src/constants.ts
@@ -0,0 +1,11 @@
+export const ENDPOINT = `/v3/fixtures?from=%s&to=%s&season=%s&league=%d`;
+
+export const LeagueCodes: Record<Results.Leagues, number> = {
+  mls: 253,
+  nwsl: 254,
+  mlsnp: 909,
+  usl1: 489,
+  usl2: 256,
+  uslc: 255,
+  nisa: 523,
+};
