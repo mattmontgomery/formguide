215f9528a874894755e1a25fafe8d0595f63c62d
Matt Montgomery <mmontgomery@deseretdigital.com>
2022-04-05 12:40:21 -0600

    fix: 🐛 adjust another constants file

--- functions/src/constants.ts
+++ functions/src/constants.ts
@@ -1,1 +1,23 @@
 export const ENDPOINT = `/v3/fixtures?from=%s&to=%s&season=%s&league=%d`;
+export const LeagueCodes: Record<Results.Leagues, number> = {
+  mls: 253,
+  nwsl: 254,
+  mlsnp: 909,
+  usl1: 489,
+  usl2: 256,
+  uslc: 255,
+  nisa: 523,
+};
+
+export const LeagueCodesInverse: Record<number, Results.Leagues> =
+  Object.entries(LeagueCodes)
+    .map(([league, code]) => ({
+      [code]: league as Results.Leagues,
+    }))
+    .reduce(
+      (acc, curr) => ({
+        ...acc,
+        ...curr,
+      }),
+      {}
+    );
