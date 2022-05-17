597e3b4be56bfe920c58aba19ac8dabe3e46c12b
Matt Montgomery <mmontgomery@deseretdigital.com>
2022-05-17 16:32:00 -0600

    fix: 🐛 renames pages to actual meanin

--- pages/[league]/facts/names-length.tsx
+++ pages/[league]/facts/names-length.tsx
@@ -4,8 +4,15 @@ import getConsecutiveGames from "@/utils/getConsecutiveGames";
 export default function MatchFactsNames(): React.ReactElement {
   return (
     <BaseGridPage
-      pageTitle="Match Facts | future matches with consecutively long names"
-      dataParser={(data) => getConsecutiveGames(data, Object.keys(data).sort())}
+      pageTitle="Match Facts | future matches with consecutive opponents by length of name"
+      dataParser={(data) =>
+        getConsecutiveGames(
+          data,
+          Object.keys(data).sort((a, b) => {
+            return a.length > b.length ? 1 : b.length > a.length ? -1 : 0;
+          })
+        )
+      }
     />
   );
 }
--- pages/[league]/facts/names-order.tsx
+++ pages/[league]/facts/names-order.tsx
@@ -4,15 +4,8 @@ import getConsecutiveGames from "@/utils/getConsecutiveGames";
 export default function MatchFactsNames(): React.ReactElement {
   return (
     <BaseGridPage
-      pageTitle="Match Facts | future matches with alphabetically consecutive names"
-      dataParser={(data) =>
-        getConsecutiveGames(
-          data,
-          Object.keys(data).sort((a, b) => {
-            return a.length > b.length ? 1 : b.length > a.length ? -1 : 0;
-          })
-        )
-      }
+      pageTitle="Match Facts | future matches with consecutively alphabetically ordered names"
+      dataParser={(data) => getConsecutiveGames(data, Object.keys(data).sort())}
     />
   );
 }
