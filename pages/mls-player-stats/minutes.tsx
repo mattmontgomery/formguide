9d3d6b1582935523bd25f422486436640b3c560f
Matt Montgomery <mmontgomery@deseretdigital.com>
2022-08-22 15:50:33 -0600

    feat: ✨ adds more minutes breakdowns

--- pages/mls-player-stats/minutes.tsx
+++ pages/mls-player-stats/minutes.tsx
@@ -36,7 +36,27 @@ export default function MLSMinutes(): React.ReactElement {
           { field: "minutes18", headerName: "<= 18" },
           { field: "minutes24", headerName: "<= 24" },
           { field: "minutes28", headerName: "<= 28" },
-          { field: "minutesPlus", headerName: "> 29" },
+          { field: "minutes29Plus", headerName: "> 29" },
+          { field: "minutes32Plus", headerName: "> 32" },
+          { field: "16" },
+          { field: "17" },
+          { field: "18" },
+          { field: "19" },
+          { field: "20" },
+          { field: "21" },
+          { field: "22" },
+          { field: "23" },
+          { field: "24" },
+          { field: "25" },
+          { field: "26" },
+          { field: "27" },
+          { field: "28" },
+          { field: "29" },
+          { field: "30" },
+          { field: "31" },
+          { field: "32" },
+          { field: "33" },
+          { field: "34" },
         ]}
         rows={Object.keys(teamMinutes).map((m) => {
           return {
@@ -44,6 +64,36 @@ export default function MLSMinutes(): React.ReactElement {
             team: m,
             all: teamMinutes[m].reduce((acc, p) => acc + p.Min, 0),
             "90s": teamMinutes[m].reduce((acc, p) => acc + Number(p["90s"]), 0),
+            ...[
+              "16",
+              "17",
+              "18",
+              "19",
+              "20",
+              "21",
+              "22",
+              "23",
+              "24",
+              "25",
+              "26",
+              "27",
+              "28",
+              "29",
+              "30",
+              "31",
+              "32",
+              "33",
+              "34",
+            ].reduce((acc: Record<string, number>, curr) => {
+              return {
+                ...acc,
+                [curr]:
+                  (acc[curr] ?? 0) +
+                  teamMinutes[m]
+                    .filter((p) => Math.floor(parseAge(p.Age)) === Number(curr))
+                    .reduce((acc, p) => acc + p.Min, 0),
+              };
+            }, {}),
             minutes18: teamMinutes[m]
               .filter((p) => parseAge(p.Age) < 19)
               .reduce((acc, p) => acc + p.Min, 0),
@@ -53,8 +103,11 @@ export default function MLSMinutes(): React.ReactElement {
             minutes28: teamMinutes[m]
               .filter((p) => parseAge(p.Age) < 29)
               .reduce((acc, p) => acc + p.Min, 0),
-            minutesPlus: teamMinutes[m]
+            minutes29Plus: teamMinutes[m]
               .filter((p) => parseAge(p.Age) > 29)
+              .reduce((acc, p) => acc + p.Min, 0),
+            minutes32Plus: teamMinutes[m]
+              .filter((p) => parseAge(p.Age) > 32)
               .reduce((acc, p) => acc + p.Min, 0),
           };
         })}
@@ -66,10 +119,4 @@ export default function MLSMinutes(): React.ReactElement {
 function parseAge(age: string): number {
   const [y, d] = age.split("-");
   return Number(y) + Number(d) / 365.25;
-}
-
-function sum<T>(field: string, data: T): (acc: number, curr: T) => number {
-  return (acc, curr) => {
-    return 0;
-  };
 }
