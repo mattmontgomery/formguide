2fee378a81f1d41cb2d95f3ae3780f2ab15cb023
Matt Montgomery <mmontgomery@deseretdigital.com>
2022-08-22 21:41:26 -0600

    feat: ✨ adds home/away ppg control to opponent ppg page

--- pages/[league]/ppg/opponent.tsx
+++ pages/[league]/ppg/opponent.tsx
@@ -1,19 +1,32 @@
 import styles from "@/styles/Home.module.css";
 import MatchCell from "@/components/MatchCell";
 import getTeamPoints from "@/utils/getTeamPoints";
-import BasePage from "@/components/BaseGridPage";
+import BaseGridPage from "@/components/BaseGridPage";
 import { getArrayAverageFormatted } from "@/utils/array";
+import { useState } from "react";
+import { FormControlLabel, FormLabel, Switch } from "@mui/material";
 
 export default function PPGOpponent(): React.ReactElement {
+  const [useHomeAway, setUseHomeAway] = useState<boolean>(true);
   return (
-    <BasePage
-      dataParser={dataParser}
+    <BaseGridPage
+      dataParser={(data) => dataParser(data, useHomeAway)}
       pageTitle="Opponent PPG before given match"
       gridClass={styles.gridWide}
-    />
+    >
+      <FormControlLabel
+        label="Use Home/Away PPG"
+        control={
+          <Switch onChange={(ev) => setUseHomeAway(ev.currentTarget.checked)} />
+        }
+      ></FormControlLabel>
+    </BaseGridPage>
   );
 }
-function dataParser(data: Results.ParsedData["teams"]): Render.RenderReadyData {
+function dataParser(
+  data: Results.ParsedData["teams"],
+  useHomeAway = true
+): Render.RenderReadyData {
   const teamPoints = getTeamPoints(data);
   return Object.keys(data).map((team) => [
     team,
@@ -32,6 +45,12 @@ function dataParser(data: Results.ParsedData["teams"]): Render.RenderReadyData {
                   opponentMatch.date < new Date(match.date) &&
                   opponentMatch.result !== null
               )
+              .filter((opponentMatch) => {
+                if (useHomeAway) {
+                  return opponentMatch.home === !match.home;
+                }
+                return true;
+              })
               .map((opponentPoints) => opponentPoints.points);
             return getArrayAverageFormatted(points);
           }}
