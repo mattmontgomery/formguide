2e2bca39e64b53343ec28dbbc0b07af9857bf860
Matt Montgomery <mmontgomery@deseretdigital.com>
2022-09-04 23:07:30 -0600

    feat: ✨ decrease browser-side cache for form api

--- utils/getExpires.ts
+++ utils/getExpires.ts
@@ -1,5 +1,5 @@
 const thisYear = new Date().getFullYear();
 
 export default function getExpires(year: number) {
-  return year === thisYear ? 60 * 60 : 60 * 60 * 24 * 7 * 4;
+  return year === thisYear ? 60 * 15 : 60 * 60 * 24 * 7 * 4;
 }
