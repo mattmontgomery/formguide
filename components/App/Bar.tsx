74456631d3799d804691888445f8063b1cbbce13
Matt Montgomery <mmontgomery@deseretdigital.com>
2026-03-01 21:06:34 -0700

    Add MenuItem for the year 2026

--- components/App/Bar.tsx
+++ components/App/Bar.tsx
@@ -95,6 +95,7 @@ export default function Bar({
           value={year}
           onChange={(ev) => onSetYear(Number(ev.target.value))}
         >
+          <MenuItem value={2026}>2026</MenuItem>
           <MenuItem value={2025}>2025</MenuItem>
           <Divider />
           <MenuItem value={2024}>2024</MenuItem>
